2634786b59891f66c8e7add85e69985bd417a882
Archana Swaminathan <swaminathan.archana11@gmail.com>
2020-01-30 22:03:04 +0530

    Add files via upload

---
+++ Assignment3/F.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int adj[10000][10000];
+int parent[100000];
+int n,grp=0;
+int  vis[100000],degree[100000],ans[100000];
+int dfs(int curr)
+{
+    printf("%d\n",curr);
+    if(curr==0)
+        return 0;
+    vis[curr]=grp;
+    for(int i=0;i<n;i++)
+    {
+        if(adj[curr][i]==1&&vis[i]!=grp)
+        {
+            return 1 + dfs(i);
+        }
+    }
+}
+int main()
+{
+    int u,v;
+    scanf("%d",&n);
+    for(int i=0;i<n-1;i++)
+    {
+        scanf("%d %d",&u,&v);
+        adj[u][v]=1;
+        adj[v][u]=1;
+        degree[u]++;
+        degree[v]++;
+    }
+    int min = 100000,index=0;
+    for(int i=0;i<n;i++)
+    {
+        if(degree[i]==1)
+        {
+            //printf("for %d\n",i);
+            grp=i;
+            ans[i] = dfs(i);
+            //printf("%d %d\n",i,ans[i]);
+            if(ans[i]<min)
+            {
+                min = ans[i];
+            }
+        }
+    }
+    int cnt=0;
+    printf("***************\n");
+    for(int i=n-1;i>=0;i--)
+    {
+        if(ans[i]==min)
+        {
+            cnt++;
+            index=i;
+        }
+    }
+    printf("%d\n%d",cnt,index);
+
+}
