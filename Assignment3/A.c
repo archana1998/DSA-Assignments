6c1fa0103f21a33598fe3e792ea4f5c01e55a757
Archana Swaminathan <swaminathan.archana11@gmail.com>
2020-01-30 12:07:08 +0530

    Update and rename x.c to A.c

---
+++ Assignment3/A.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+void swap(int *xp, int *yp) 
+{ 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+  
+
+void bubbleSort(int arr[], int n) 
+{ 
+   int i, j; 
+   for (i = 0; i < n-1; i++)       
+    {
+       for (j = 0; j < n-i-1; j++)  
+       {
+           if (arr[j] > arr[j+1]) 
+              swap(&arr[j], &arr[j+1]); 
+       }
+    }
+} 
+
+int main()
+{
+   int N,index;
+   int count = 1;
+   int max=0;
+   scanf("%d",&N);
+   int A[N+1];
+   for(int i =0;i<N;i++)
+    {
+       scanf("%d",&A[i]);
+    }
+    bubbleSort(A,N);
+    for(int i=0;i<N;i++)
+    {
+        if(A[i]==A[i+1])
+            count++;
+        else
+        {
+            if(count>max)
+            {
+                max=count;
+                index = A[i];
+            }
+            count = 1;
+        }
+    }
+    if(max%2 ==0 && max>=N/2)
+        printf("%d",index);
+    else
+    {
+        if(max>=(N+1)/2)
+            printf("%d",index);
+        else
+            printf("NO MAJORITY ELEMENT");
+    }
+    return 0;
+}
+   
